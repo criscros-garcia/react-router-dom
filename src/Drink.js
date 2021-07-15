73b6c0f2f435e1d087d568ba9e30f62aa5f44fcc
Cristian Garcia <eminemko@hotmail.com>
2021-07-15 16:48:51 -0500

    Create Home, Eat, and Drink components

---
+++ src/Drink.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Drink extends Component{
+render(){
+  return(
+    <div>
+      <h1>Drink</h1>
+    </div>
+  );
+}
+}
+
+export default Drink;
---
+++ src/Eat.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Eat extends Component{
+render(){
+  return(
+    <div>
+      <h1>Eat</h1>
+    </div>
+  );
+}
+}
+
+export default Eat;
