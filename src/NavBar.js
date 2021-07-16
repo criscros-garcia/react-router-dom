d04e11afbc9801c3de8bcefe6b89c9e657a43043
Cristian Garcia <eminemko@hotmail.com>
2021-07-15 19:17:06 -0500

    Remove underline from links

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,20 @@
+import { transform } from "@babel/core";
 import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
+import "./NavBar.css";
 
 
 class NavBar extends Component {
   render() {
     const activeStyle = {
       fontWeight: "bold",
-      color: "mediumorchid"
+      color: "mediumorchid",
     };
     return (
         <nav>
-          <NavLink exact to="/"      activeStyle={activeStyle}> Home </NavLink>
-          <NavLink exact to="/eat"   activeStyle={activeStyle}> Eat </NavLink>
-          <NavLink exact to="/drink" activeStyle={activeStyle}> Drink </NavLink>
+          <NavLink exact to="/"      className='link' activeStyle={activeStyle} > Home </NavLink>
+          <NavLink exact to="/eat"   className='link' activeStyle={activeStyle} > Eat </NavLink>
+          <NavLink exact to="/drink" className='link' activeStyle={activeStyle} > Drink </NavLink>
         </nav>
     );
   }
